023606eb1fe77f2990a49b5a6668ff1f969650cb
Marat Shakirov <marat@shakirov.name>
2025-05-03 12:27:35 +0300

    Add PGP encryption/decryption stubs for future integration

---
+++ utils/pgp.go
@@ -0,0 +1,17 @@
+package utils
+
+import "fmt"
+
+// EncryptPGP – временная заглушка (эмуляция шифрования)
+func EncryptPGP(plaintext string) (string, error) {
+	return fmt.Sprintf("pgp{%s}", plaintext), nil
+}
+
+// DecryptPGP – временная заглушка (эмуляция расшифровки)
+func DecryptPGP(ciphertext string) (string, error) {
+	// Снимаем обёртку вида pgp{...}
+	if len(ciphertext) >= 5 && ciphertext[:4] == "pgp{" && ciphertext[len(ciphertext)-1:] == "}" {
+		return ciphertext[4 : len(ciphertext)-1], nil
+	}
+	return ciphertext, nil
+}
